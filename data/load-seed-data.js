703bd33bbf06c75b55fbde70996f5533ff9c572d
Gabriel Simek <gabrielsimek@outlook.com>
2021-05-06 17:31:53 -0700

    add user and seed user

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -2,6 +2,7 @@
 import client from '../lib/client.js';
 // import our seed data:
 import countries from './countries.js';
+import users from './users.js';
 
 
 run();
@@ -10,13 +11,27 @@ async function run() {
 
   try {
 
+    const data = await Promise.all(
+      users.map((user) => {
+        return client.query(`
+        INSERT INTO users (name, email, password_hash)
+        VALUES ($1, $2, $3)
+        RETURNING *;
+        `,
+        [user.name, user.email, user.password]);
+      })
+    );
+
+    const user = data[0].rows[0];
+    console.log(user);
+
     await Promise.all(
       countries.map(country => {
         return client.query(`
-          INSERT INTO countries (name, language, president, capital, url, population, has_mcdonald)
-          VALUES ($1, $2, $3, $4, $5, $6, $7);
+          INSERT INTO countries (name, language, president, capital, url, population, has_mcdonald, user_id)
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
         `,
-        [country.name, country.language, country.president,  country.capital, country.url, country.population, country.hasMcdonald]);
+        [country.name, country.language, country.president,  country.capital, country.url, country.population, country.hasMcdonald, user.id]);
       })
     );
     
---
+++ data/users.js
@@ -0,0 +1,7 @@
+let users = [{
+  name: 'Me the User',
+  email: 'me@user.com',
+  password: 'password'
+}];
+
+export default users;
